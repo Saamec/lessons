a186fce96fa4a21c83022c64c075097004efcb28
«Ilya <333zero777@mail.ru>
2022-10-16 20:58:53 +0300

    3 Tasks

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println(Threelevel(5));
         System.out.println(Factorial(5));
+        double variable = Calc();
+        System.out.println(variable);
 
 
     }
@@ -20,4 +25,38 @@ public class Main {
             return n * Factorial(n - 1);
         }
     }
+    public static double Calc() throws IOException {
+        double result = 0;
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Введите через ентер число, потом оператор, потом число");
+        Double first = s.nextDouble();
+
+        String operator = s.next();
+
+        Double second = s.nextDouble();
+
+        switch (operator){
+            case ("+"):
+                    result = first + second;
+                    break;
+            case ("-"):
+                result = first - second;
+                break;
+            case ("*"):
+                result = first * second;
+                break;
+            case ("/"):
+                if (second != 0) {
+                    result = first / second;
+                }
+                else System.out.println("Делить на 0 нельзя!");
+                break;
+
+        }
+        return result;
+    };
+
+
+
 }
